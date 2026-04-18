741f607716157666d00b75bf5b3aa2ced52c1eca
Shad Torrie <shad.torrie@gmail.com>
2026-04-18 09:24:52 -0600

    Update service worker build version

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const BUILD_VERSION = 'git-866a0177bdc7';
+const BUILD_VERSION = 'git-9a24f43c4abe';
 const PRECACHE_CORE = `precache-core-${BUILD_VERSION}`;
 const PRECACHE_CARD = `precache-card-${BUILD_VERSION}`;
 const RUNTIME_CACHE = `runtime-${BUILD_VERSION}`;
